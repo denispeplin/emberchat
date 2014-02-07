4f61e6a61d3837ba5f5d44781b3392c672d0f72d
denispeplin <denis.peplin@gmail.com>
2014-02-07 23:36:40 +0400

    Implement Message model, add messages to the list using form.

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-App = Ember.Application.create({});
+App = Ember.Application.create({
+  LOG_ACTIVE_GENERATION: true,
+  LOG_TRANSITIONS: true
+});
+
+App.Router.map(function() {
+  this.resource('messages');
+});
+
+App.MessagesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('message');
+  }
+});
+
+App.ApplicationController = Ember.Controller.extend({
+  init: function() {
+    this.transitionToRoute('messages');
+  }
+});
+
+App.MessagesController = Ember.ArrayController.extend({
+  actions: {
+    create: function() {
+      var data = this.getProperties('body');
+      this.set('body', '');
+      this.store.createRecord('message', data);
+    }
+  }
+});
+
+App.Message = DS.Model.extend({
+  body: DS.attr('string')
+});
+
+App.Store = DS.Store.extend({
+  adapter: DS.FixtureAdapter
+});
+
+App.Message.FIXTURES = [
+]
